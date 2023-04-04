489d14d0f147cdde079ea60e1e195e17bfea5d87
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-05 00:20:15 +0200

    up

--- pipelines/examples/hello.groovy
+++ pipelines/examples/hello.groovy
@@ -9,7 +9,9 @@ node(){
             userRemoteConfigs: [[url: 'https://github.com/alpeza/DummyWeb.git']],
             extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'repo']]
         ])
+        sh "ls -a; echo '.....'"
         dir('repo'){
+            sh "ls -a"
             load('Jenkinsfile')
         }
     }
