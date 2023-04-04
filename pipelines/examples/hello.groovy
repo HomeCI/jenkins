b28b077258af23e220b3cf854aa4016822ecdc15
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-04 23:48:35 +0200

    up

--- pipelines/examples/hello.groovy
+++ pipelines/examples/hello.groovy
@@ -1,5 +1,15 @@
 library 'hci@main'
 
 node(){
-    docker tagname: "mypipeline:1.0.0", dbuild: false, pullToRegistry: false
+    stage('Clonando repositorio'){
+        checkout([ $class: 'GitSCM', branches: [[name: 'main']], doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'jenkins']],
+        submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/alpeza/DummyWeb.git']]
+        ])
+
+        def pipelineScript = "Jenkinsfile"
+        def scmVars = checkout(scm).getEnvVars()
+        load(pipelineScript)
+    }
+    //docker tagname: "mypipeline:1.0.0", dbuild: false, pullToRegistry: false
 }
