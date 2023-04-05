b9c41dfd68a8c5b1676559c34ab4a4b26a9af98e
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-05 21:41:45 +0200

    aquii

---
+++ dockerBuild.groovy
@@ -0,0 +1,26 @@
+pipelineJob('DockerBuild') {
+    description('Pipeline encargada del build, despliegue y promoción de imágenes Docker')
+    parameters {
+        string(name: 'tagname', defaultValue: '', description: 'Nombre de la etiqueta para la imagen de Docker')
+        booleanParam(name: 'deploy', defaultValue: true, description: '¿Realizar el despliegue de la imagen de Docker?')
+        booleanParam(name: 'dbuild', defaultValue: true, description: '¿Construir la imagen de Docker?')
+        string(name: 'dockerfilePath', defaultValue: '.', description: 'Ruta del archivo Dockerfile')
+        string(name: 'dockercomposePath', defaultValue: '.', description: 'Ruta del archivo docker-compose')
+        string(name: 'credentialsId', defaultValue: '', description: 'ID de las credenciales para el registro de Docker')
+        booleanParam(name: 'pullToRegistry', defaultValue: false, description: '¿Realizar la carga de la imagen a un registro de Docker?')
+    }
+    stages {
+        stage('Ejemplo de pipeline') {
+            steps {
+                // Aquí puedes agregar los pasos que quieras para construir y desplegar tu imagen de Docker
+                echo "El valor de tagname es: ${params.tagname}"
+                echo "El valor de deploy es: ${params.deploy}"
+                echo "El valor de dbuild es: ${params.dbuild}"
+                echo "La ruta del Dockerfile es: ${params.dockerfilePath}"
+                echo "La ruta del archivo docker-compose es: ${params.dockercomposePath}"
+                echo "El ID de las credenciales es: ${params.credentialsId}"
+                echo "El valor de pullToRegistry es: ${params.pullToRegistry}"
+            }
+        }
+    }
+}
