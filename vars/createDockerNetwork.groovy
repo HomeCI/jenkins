7e3fb54b403ed2b0fb49dc53ffcbec5a1ba65d2a
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-05 20:35:27 +0200

    aquii

--- vars/createDockerNetwork.groovy
+++ vars/createDockerNetwork.groovy
@@ -1,5 +1,6 @@
 def call(Map params) {
-    String name = params.tagname != null ? params.name : "core_npm_hcinet"
+    String name = "core_npm_hcinet"
+    if(params) name = params.name != null ? params.name : name
     print("Tratando de crear la red: ${name}")
     sh """
     docker network create --driver bridge --attachable ${name} || true
