20bfba9d680cfbd018cc54657f3aadb2f744811b
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-07 01:12:24 +0200

    update

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,8 +1,13 @@
+import hudson.model.BooleanParameterValue
 import hudson.model.StringParameterValue
 
 def call(Map params) {
     def parameterValueList = params.collect { key, value ->
-        new StringParameterValue(key, value)
+        if (value instanceof Boolean) {
+            new BooleanParameterValue(key, value)
+        } else {
+            new StringParameterValue(key, value.toString())
+        }
     }
     build job: 'DockerBuild', parameters: parameterValueList
 }
