0940a976ae7875e66c516713e602aa4607eb3918
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-07 01:10:04 +0200

    update

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,4 +1,8 @@
+import hudson.model.StringParameterValue
+
 def call(Map params) {
-    def linkedHashMapParams = params as LinkedHashMap
-    build job: 'DockerBuild', parameters: linkedHashMapParams
+    def parameterValueList = params.collect { key, value ->
+        new StringParameterValue(key, value)
+    }
+    build job: 'DockerBuild', parameters: parameterValueList
 }
