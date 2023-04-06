d636de325f720381cc19224a7018dce382fe2c18
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-06 19:55:55 +0200

    update

--- pipelines/webhook/webhookHandler.groovy
+++ pipelines/webhook/webhookHandler.groovy
@@ -3,7 +3,7 @@ library 'hci@main'
 node(){
     stage('Clonando repositorio'){
         sh "echo $ref"
-        sh "cat refs/heads/main"
+       
         sh "echo '----------------'"
         sh "env"
         sh '''
