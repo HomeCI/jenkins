c8b18b6000e9e732396d49cdea667610b3d53b0e
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-07 10:09:35 +0200

    update

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,7 +4,7 @@ import hudson.model.StringParameterValue
 def call(Map params) {
     
     gitRepository = sh ( script: "echo $ref_repository_clone_url", returnStdout: true ).trim()
-    params['gitrepourl'] = gitRepositorygitRepository
+    params['gitrepourl'] = gitRepository
 
     def parameterValueList = params.collect { key, value ->
         if (value instanceof Boolean) {
